8d9348c62071b398b63cc79f18de17615f5d6d3b
Devpatel1901 <entrepreneurdev1901@gmail.com>
2025-09-21 15:29:51 -0400

    Completed one version of the game

--- game.go
+++ game.go
@@ -4,34 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 
-	"slices"
-
 	"github.com/Devpatel1901/cards/v2"
 )
-
-func calculateScore(deckOfCards []cards.Card) int {
-	total := 0
-	numberOfAce := 0
-	for _, c := range deckOfCards {
-		if c.Rank >= cards.Ten && c.Rank <= cards.King {
-			total += 10
-		} else if c.Rank == cards.Ace {
-			numberOfAce += 1
-		} else {
-			total = total + int(c.Rank)
-		}
-	}
-
-	for range numberOfAce {
-		if 21-total >= 11 {
-			total += 11
-		} else {
-			total += 1
-		}
-	}
-
-	return total
-}
 
 func initializePlayers() []Player {
 	var players []Player
@@ -41,29 +15,14 @@ func initializePlayers() []Player {
 	return players
 }
 
-func drawCardFromDeck(deckOfCards *[]cards.Card) cards.Card {
-	if len(*deckOfCards) == 0 {
-		panic("deck is empty")
+func draw(deck []cards.Card) (cards.Card, []cards.Card, error) {
+	if len(deck) == 0 {
+		return cards.Card{}, deck, fmt.Errorf("deck is empty")
 	}
-
-	idx := rand.Intn(len(*deckOfCards))
-	card := (*deckOfCards)[idx]
-
-	*deckOfCards = slices.Delete((*deckOfCards), idx, idx+1)
-
-	return card
+	return deck[0], deck[1:], nil
 }
 
-func dealInitialCards(players []Player, deckOfCards []cards.Card, cardsPerPlayer int) {
-	for range cardsPerPlayer {
-		for i := range players {
-			card := drawCardFromDeck(&deckOfCards)
-			players[i].AddCard(card)
-		}
-	}
-}
-
-func showInitialCards(players []Player) {
+func showPlayerCards(players []Player) {
 	for i := range players {
 		fmt.Printf("--------------%v CARDS--------------\n", players[i].Name())
 		players[i].Show()
@@ -82,12 +41,74 @@ func hideOneCardFromDealerHands(players []Player) {
 }
 
 func StartGame() {
-	finalDeck := cards.FromDecks(cards.NewDeck(), cards.NewDeck())
-
+	deckOfCards := cards.FromDecks(cards.NewDeck(cards.Shuffle), cards.NewDeck(cards.Shuffle))
+	fmt.Println(len(deckOfCards))
 	players := initializePlayers()
 
-	dealInitialCards(players, finalDeck, 2)
+	var card cards.Card
+	var err error
+	for j := 0; j < 2; j++ {
+		for i := range players {
+			card, deckOfCards, err = draw(deckOfCards)
+			if err != nil {
+				fmt.Println("No more cards left in deck!")
+				break
+			}
+			players[i].AddCard(card)
+		}
+	}
+
 	hideOneCardFromDealerHands(players)
 
-	showInitialCards(players)
+	var input string
+
+	for i := range len(players) - 1 {
+		for input != "s" {
+			showPlayerCards(players)
+
+			fmt.Printf("What will you do %v? (h)it, (s)tand: ", players[i].Name())
+			fmt.Scanf("%s\n", &input)
+
+			switch input {
+			case "h":
+				card, deckOfCards, err = draw(deckOfCards)
+				if err != nil {
+					fmt.Println("No more cards left in deck!")
+					break
+				}
+				players[i].AddCard(card)
+			}
+		}
+		input = ""
+	}
+
+	dealerScore, isSoftScore := players[len(players)-1].Score()
+
+	for dealerScore <= 16 || (dealerScore == 17 && isSoftScore) {
+		card, deckOfCards, err = draw(deckOfCards)
+		if err != nil {
+			fmt.Println("No more cards left in deck!")
+			break
+		}
+		players[len(players)-1].AddCard(card)
+	}
+
+	var maxScore int
+	var winner string
+
+	for i := range players {
+		pScore, _ := players[i].Score()
+
+		if pScore > 21 {
+			fmt.Printf("%v busted\n", players[i].Name())
+			continue
+		}
+
+		if pScore > maxScore {
+			maxScore = pScore
+			winner = players[i].Name()
+		}
+	}
+
+	fmt.Printf("%v win!!!", winner)
 }
--- player.go
+++ player.go
@@ -33,3 +33,28 @@ func (p Player) Hand() []cards.Card {
 func (p *Player) AddCard(cardsToAdd ...cards.Card) {
 	p.hand = append(p.hand, cardsToAdd...)
 }
+
+func (p Player) Score() (int, bool) {
+	total := 0
+	numberOfAce := 0
+	softScore := false
+
+	for _, c := range p.hand {
+		switch {
+		case c.Rank >= cards.Ten && c.Rank <= cards.King:
+			total += 10
+		case c.Rank == cards.Ace:
+			numberOfAce++
+			total += 1
+		default:
+			total += int(c.Rank)
+		}
+	}
+
+	if numberOfAce > 0 && total+10 <= 21 {
+		total += 10
+		softScore = true
+	}
+
+	return total, softScore
+}
